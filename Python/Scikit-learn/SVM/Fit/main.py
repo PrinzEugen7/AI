2d4c671e2bd6266770eae1aab355039f6d0a75e9
PrinzEugen7 <nsi05162@pooae.com>
2016-12-04 18:29:04 +0900

    Update and rename Python/Scikit-learn/SVM/Test/main.py to Python/Scikit-learn/SVM/Fit/main.py

--- Python/Scikit-learn/SVM/Fit/main.py
+++ Python/Scikit-learn/SVM/Fit/main.py
@@ -8,10 +8,8 @@ def main():
     target = digits.target[:-1]
     # サポートベクターマシンで教師あり学習
     clf = svm.SVC(gamma=0.001, C=100.)
-    clf.fit(digits.data[:-1], target)  
-    # 推定
-    ans = clf.predict(digits.data[-1])
-    print(ans)
+    clf.fit(digits.data[:-1], digits.target[:-1])  
+    print(clf)
 
 if __name__ == "__main__":
     main()
